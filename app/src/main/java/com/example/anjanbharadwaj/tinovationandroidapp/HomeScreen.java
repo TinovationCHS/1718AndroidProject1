21460c56192bec2d54fc39966f7233964186aa94
hydefbla <anjanbharadwaj02@gmail.com>
2017-11-04 11:36:07 -0700

    Database code

--- app/src/main/java/com/example/anjanbharadwaj/tinovationandroidapp/HomeScreen.java
+++ app/src/main/java/com/example/anjanbharadwaj/tinovationandroidapp/HomeScreen.java
@@ -2,9 +2,17 @@ package com.example.anjanbharadwaj.tinovationandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class HomeScreen extends AppCompatActivity {
     DatabaseReference root = FirebaseDatabase.getInstance().getReference();
@@ -12,9 +20,24 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        User user = new User("Ranesh","Islazy");
-        root.setValue(user);
 
+        root.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Set<String> set = new HashSet<String>();
+                Iterator i = dataSnapshot.getChildren().iterator();
+                while(i.hasNext()){
+                    String key = (((DataSnapshot) i.next()).getKey());
+                    String value = ((dataSnapshot).child(key).getValue().toString());
+                    set.add(key+" has a value of  "+value);
+                }
 
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }

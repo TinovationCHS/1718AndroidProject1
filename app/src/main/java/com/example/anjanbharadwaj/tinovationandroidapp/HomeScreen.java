ea76529d86bed65985c6bab06c57c1ec8c46847c
hydefbla <anjanbharadwaj02@gmail.com>
2017-11-04 10:43:22 -0700

    Firebase Database

--- app/src/main/java/com/example/anjanbharadwaj/tinovationandroidapp/HomeScreen.java
+++ app/src/main/java/com/example/anjanbharadwaj/tinovationandroidapp/HomeScreen.java
@@ -3,11 +3,18 @@ package com.example.anjanbharadwaj.tinovationandroidapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeScreen extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class HomeScreen extends AppCompatActivity {
+    DatabaseReference root = FirebaseDatabase.getInstance().getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        User user = new User("Ranesh","Islazy");
+        root.setValue(user);
+
+
     }
 }
---
+++ app/src/main/java/com/example/anjanbharadwaj/tinovationandroidapp/User.java
@@ -0,0 +1,19 @@
+package com.example.anjanbharadwaj.tinovationandroidapp;
+
+/**
+ * Created by anjanbharadwaj on 11/4/17.
+ */
+
+public class User {
+
+    public String username;
+    public String email;
+
+
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+}

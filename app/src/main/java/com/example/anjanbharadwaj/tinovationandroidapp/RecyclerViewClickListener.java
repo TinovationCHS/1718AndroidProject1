ea07701ce62936bbc06bbfd4f41c879b1d2be728
chsfblamobileapp2018 <chsfblamobileapp2018@gmail.com>
2018-02-17 13:46:48 -0800

    Pushing the codebase

---
+++ app/src/main/java/com/example/anjanbharadwaj/tinovationandroidapp/RecyclerViewClickListener.java
@@ -0,0 +1,12 @@
+package com.example.anjanbharadwaj.tinovationandroidapp;
+
+/**
+ * Created by anjanbharadwaj on 2/17/18.
+ */
+
+import android.view.View;
+
+public interface RecyclerViewClickListener {
+
+    void onClick(View view, int position);
+}

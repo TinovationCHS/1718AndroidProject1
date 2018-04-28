f5b3131fb39bda0e3c0ba68c19d7301c5cee2ad2
anjanbharadwaj <anjanbharadwaj02@gmail.com>
2018-04-28 11:29:03 -0700

    Main Screen and Functionality of Application working

--- app/src/main/java/com/example/anjanbharadwaj/androidapp/Subject.java
+++ app/src/main/java/com/example/anjanbharadwaj/androidapp/Subject.java
@@ -24,6 +24,7 @@ public class Subject implements Parcelable{
         return classDescription;
     }
 
+
     public void setClassDescription(String classDescription) {
         this.classDescription = classDescription;
     }
